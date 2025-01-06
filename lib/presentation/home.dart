f77e298f9520107c5e34cf0d2f695c0845498299
mouadMBL <159123495+mouadMBL@users.noreply.github.com>
2025-01-06 20:29:40 +0100

    feat: add custom bottom navigation bar widget

---
+++ lib/presentation/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home Page',
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ),
+    Text(
+      'Search Page',
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ),
+    Text(
+      'Profile Page',
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation Bar'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed, 
+      ),
+    );
+  }
+}
